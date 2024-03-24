371c0532ac4c3b9a697bad760fe9070f7ba141e6
Camilo Figueroa <cigotete@gmail.com>
2024-03-24 18:17:09 -0500

    Customizing Webpack of root-config to generate centralized index.html

--- packages/root-config/webpack.config.js
+++ packages/root-config/webpack.config.js
@@ -1,9 +1,9 @@
-const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-ts");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "ventas";
+  const isLocal = webpackConfigEnv && webpackConfigEnv.isLocal;
   const defaultConfig = singleSpaDefaults({
     orgName,
     projectName: "root-config",
@@ -12,17 +12,22 @@ module.exports = (webpackConfigEnv, argv) => {
     disableHtmlGeneration: true,
   });
 
-  return merge(defaultConfig, {
-    // modify the webpack config however you'd like to by adding to this object
+  const final = {
+    ...defaultConfig,
     plugins: [
+      ...defaultConfig.plugins,
       new HtmlWebpackPlugin({
         inject: false,
+        filename: "index.html",
         template: "src/index.ejs",
         templateParameters: {
-          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          isLocal,
           orgName,
-        },
-      }),
-    ],
-  });
+          FEATURE_APP_DATA: process.env.FEATURE_APP_DATA
+        }
+      })
+    ]
+  };
+
+  return final;
 };
